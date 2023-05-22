b051c7c14d20c1ef6460076a067c9b14affb3b57
MichaelStarkman <michaelstarkman@gmail.com>
2023-05-22 13:51:36 -0600

    serverCrashed

--- routes/profiles.js
+++
@@ -1,9 +0,0 @@
-// const express = require("express");
-// const profilesCtrl = require("../controllers/profiles");
-
-// const profilesRouter = express.Router();
-
-// // POST /profiles (create a profile -after sign up)
-// profilesRouter.post("/", profilesCtrl.create);
-
-// module.exports = profilesRouter;
