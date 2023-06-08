db2cef9bfae0221548d3ff299f770e7a0de57df9
krsnamara <martinj.fitzpatrick@gmail.com>
2023-06-08 02:12:20 -0400

    working in the s3 and mongo image upload to events

--- models/events.js
+++ models/events.js
@@ -1,21 +1,21 @@
 const mongoose = require("mongoose");
 
 const EventsSchema = new mongoose.Schema(
-    {
-        name: { type: String, required: true },
-        eventType: { type: Array, required: true},
-        image: { type: String, required: true},
-        address: { type: String, required: true },
-        description: { type: String, required: true},
-        reservation: { type: String, required: true},
-        website: { type: String, required: true},
-        suitability: { type: Array},
-        amenities: { type: Array},
-        categories: { type: Array},
-        foodNightlife: { type: Array },
-        attractions: { type: Array },
-    },
-    {timestamps: true}
+  {
+    name: { type: String, required: true },
+    eventType: { type: Array, required: true },
+    imageName: { type: String },
+    address: { type: String, required: true },
+    description: { type: String, required: true },
+    reservation: { type: String, required: true },
+    website: { type: String, required: true },
+    suitability: { type: Array },
+    amenities: { type: Array },
+    categories: { type: Array },
+    foodNightlife: { type: Array },
+    attractions: { type: Array },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("Events", EventsSchema);
