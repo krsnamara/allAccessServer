fb1d799a2500d2eb0d1d96646d7135b998067dfc
krsnamara <martinj.fitzpatrick@gmail.com>
2023-06-08 19:52:08 -0400

    updating the get route mongoose query

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,75 +1,85 @@
-const express = require('express');
+const express = require("express");
 const reviewsRouter = express.Router();
-const isAuthenticated = require('../utils/isAuth.js');
-const Reviews = require('../models/reviews.js')
+const Reviews = require("../models/reviews.js");
 
 // Routes //
 
-// REVIEWS INDEX ROUTE
-reviewsRouter.get("/", async (req, res) => {
-    try {
-        // send all reviews
-        // if(req.user) {
-            res.json(await Reviews.find());
-        // } else {
-        //     res.json(await Reviews.find());
-        // }
-    } catch (error) {
-        // send error
-        res.status(400).json(error);
-    }
+// Seed
+const seed = require("../data/reviewSeed.js");
+reviewsRouter.get("/seed", async (req, res) => {
+  try {
+    await Reviews.deleteMany({});
+    const data = await Reviews.create(seed);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error); // Log the error for debugging purposes
+    res.status(500).json({ error: "Failed to seed data" });
+  }
 });
 
+// REVIEWS INDEX ROUTE
+reviewsRouter.get("/", async (req, res) => {
+  try {
+    // send all reviews
+    // if(req.user) {
+    res.json(await Reviews.find());
+    // } else {
+    //     res.json(await Reviews.find());
+    // }
+  } catch (error) {
+    // send error
+    res.status(400).json(error);
+  }
+});
 
 // REVIEWS CREATE ROUTE
 reviewsRouter.post("/", async (req, res) => {
-    try {
-        // take authenticated user id and attach to request body
-        req.body.uid = req.user.uid;
-        // send all reviews
-        const reviews = await Reviews.create(req.body);
-        
-        res.json(reviews);
-    } catch (error) {
-        //send error
-        res.status(400).json(error);
-    }
+  try {
+    // take authenticated user id and attach to request body
+    req.body.uid = req.user.uid;
+    // send all reviews
+    const reviews = await Reviews.create(req.body);
+
+    res.json(reviews);
+  } catch (error) {
+    //send error
+    res.status(400).json(error);
+  }
 });
 
 // REVIEWS DELETE ROUTE
 reviewsRouter.delete("/:id", async (req, res) => {
-    try {
-        // send all reviews
-        res.json(await Reviews.findByIdAndRemove(req.params.id));
-        
-    } catch (error) {
-        // send error
-        res.status(400).json(error);
-    }
+  try {
+    // send all reviews
+    res.json(await Reviews.findByIdAndRemove(req.params.id));
+  } catch (error) {
+    // send error
+    res.status(400).json(error);
+  }
 });
 
 // REVIEWS UPDATE ROUTE
 reviewsRouter.put("/:id", async (req, res) => {
-    try {
-        req.body.uid = req.user.uid;
-        //send all reviews
-        res.json(
-            await Reviews.findByIdAndUpdate(req.params.id, req.body, { new: true})
-            );
-        } catch (error) {
-            // send error
-            res.status(400).json(error);
-        }
-    });
-    
-    // REVIEW SHOW ROUTE
-    reviewsRouter.get("/:id", async (req, res) => {
-        try {
-                res.json(await Reviews.findByIdreq.params.id());
-        } catch (error) {
-            // send error
-            res.status(400).json(error);
-        }
-    });
-    
-    module.exports = reviewsRouter;
+  try {
+    req.body.uid = req.user.uid;
+    //send all reviews
+    res.json(
+      await Reviews.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (error) {
+    // send error
+    res.status(400).json(error);
+  }
+});
+
+// REVIEW SHOW ROUTE
+reviewsRouter.get("/:id", async (req, res) => {
+  try {
+    res.json(await Reviews.findByIdreq.params.id());
+  } catch (error) {
+    // send error
+    res.status(400).json(error);
+  }
+});
+
+module.exports = reviewsRouter;
---
+++ data/reviewSeed.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    name: "Willow Smith",
+    comment:
+      "Cooking Beyond Barriers has been a game-changer for me. As someone with a physical disability, I've always felt discouraged in the kitchen. But this class opened up a whole new world of possibilities.",
+    imageName: "https://loremflickr.com/320/240",
+  },
+  {
+    name: "Steve Rogers",
+    comment:
+      "Great place for some lorem ipsum for pabst blue twin wrapping around the places of spaces we've been.",
+  },
+  {
+    name: "Natasha Romanoff",
+    comment:
+      "Joining the Cooking Beyond Barriers class has been one of the best decisions I've made. As a person with limited mobility, I never thought I could enjoy cooking. But the instructors showed me how to adapt and modify recipes to fit my abilities.",
+  },
+  {
+    name: "Sophia",
+    comment:
+      "Wheelies had a lot of accommodations for me and I felt like I was able navigate the venue with ease. The atmosphere is warm, friendly, and non-judgmental.",
+  },
+];
