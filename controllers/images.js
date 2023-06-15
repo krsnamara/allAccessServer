77d75b272ce90fe9c2de6c4238546b4b1f969abc
krsnamara <martinj.fitzpatrick@gmail.com>
2023-06-15 00:35:57 -0400

    updating backend for signurl cdn connection

--- controllers/images.js
+++ controllers/images.js
@@ -5,21 +5,20 @@ const Images = require("../models/images.js");
 
 // Specific imports for image upload
 const multer = require("multer");
-// const crypto = require("crypto");
-// const sharp = require("sharp");
+const crypto = require("crypto");
+const sharp = require("sharp");
 
 // initialing the aws sdk and cloudfront signer
 const {
   S3Client,
-  // PutObjectCommand,
+  PutObjectCommand,
   DeleteObjectCommand,
 } = require("@aws-sdk/client-s3");
-
 const { getSignedUrl } = require("@aws-sdk/cloudfront-signer");
 
 // for naming the images uploaded with unique names
-// const randomImageName = (bytes = 32) =>
-//   crypto.randomBytes(bytes).toString("hex");
+const randomImageName = (bytes = 32) =>
+  crypto.randomBytes(bytes).toString("hex");
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const bucketRegion = process.env.AWS_BUCKET_REGION;
@@ -37,7 +36,7 @@ const s3 = new S3Client({
 
 // setting up multer to handle the image upload and memory storage
 const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
+const upload = multer({ storage: storage });
 
 // cors middleware to allow access to the api from the frontend
 imagesRouter.use((req, res, next) => {
@@ -50,23 +49,23 @@ imagesRouter.use((req, res, next) => {
 // TODO: Add authentication middleware
 // TODO: Add seed data routes to repopulate the database
 
-// Get all posts
-
 imagesRouter.get("/", async (req, res) => {
   try {
-    const posts = await Images.find().sort({ _id: -1 });
+    const images = await Images.find().sort({ createdAt: -1 });
 
-    for (const post of posts) {
-      post.imageUrl = getSignedUrl({
-        url: "https://d43rby6106out.cloudfront.net/" + post.imageName,
+    const updatedImages = [];
+    for (const image of images) {
+      const imageObject = image.toObject(); // Convert Mongoose document to plain JavaScript object
+      imageObject.imageUrl = getSignedUrl({
+        url: "https://d43rby6106out.cloudfront.net/" + image.imageName,
         dateLessThan: new Date(Date.now() + 1000 * 60 * 60 * 24),
         privateKey: process.env.CLOUDFRONT_PRIVATE_KEY,
         keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID,
       });
-      console.log(process.env.CLOUDFRONT_KEY_PAIR_ID);
+      updatedImages.push(imageObject); // Push the updated imageObject to the new array
     }
 
-    res.send(posts);
+    res.send(updatedImages); // Send the updatedImages array to the frontend
   } catch (error) {
     console.error(error);
     res.status(500).send({ message: "Internal Server Error" });
@@ -75,10 +74,27 @@ imagesRouter.get("/", async (req, res) => {
 
 // Post a new post
 
-imagesRouter.post("/post", async (req, res) => {
+imagesRouter.post("/", upload.single("image"), async (req, res) => {
   try {
+    const buffer = await sharp(req.file.buffer)
+      .resize({ height: 1920, width: 1080, fit: "contain" })
+      .toBuffer();
+
+    const imageName = randomImageName();
+
+    const params = {
+      Bucket: bucketName,
+      Key: imageName,
+      Body: buffer,
+      ContentType: req.file.mimetype,
+    };
+
+    const command = new PutObjectCommand(params);
+    await s3.send(command);
+
     const post = new Images({
       name: req.body.name,
+      imageName: imageName,
       description: req.body.description,
       address: req.body.address,
       description: req.body.description,
@@ -101,77 +117,9 @@ imagesRouter.post("/post", async (req, res) => {
   }
 });
 
-// With S3 image upload
-
-// imagesRouter.get("/", async (req, res) => {
-//   try {
-//     const posts = await Images.find().sort({ _id: -1 });
-
-//     for (const post of posts) {
-//       post.imageUrl = getSignedUrl({
-//         url: "https://d43rby6106out.cloudfront.net/" + post.imageName,
-//         dateLessThan: new Date(Date.now() + 1000 * 60 * 60 * 24),
-//         privateKey: process.env.CLOUDFRONT_PRIVATE_KEY,
-//         keyPairId: process.env.CLOUDFRONT_KEY_PAIR_ID,
-//       });
-//       console.log(process.env.CLOUDFRONT_KEY_PAIR_ID);
-//     }
-
-//     res.send(posts);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send({ message: "Internal Server Error" });
-//   }
-// });
-
-// // Post a new post
-
-// imagesRouter.post("/post", upload.single("image"), async (req, res) => {
-//   try {
-//     const buffer = await sharp(req.file.buffer)
-//       .resize({ height: 1920, width: 1080, fit: "contain" })
-//       .toBuffer();
-
-//     const imageName = randomImageName();
-
-//     const params = {
-//       Bucket: bucketName,
-//       Key: imageName,
-//       Body: buffer,
-//       ContentType: req.file.mimetype,
-//     };
-
-//     const command = new PutObjectCommand(params);
-//     await s3.send(command);
-
-//     const post = new Images({
-//       name: req.body.name,
-//       imageName: imageName,
-//       description: req.body.description,
-//       address: req.body.address,
-//       description: req.body.description,
-//       reservation: req.body.reservation,
-//       website: req.body.website,
-//       suitability: req.body.suitability,
-//       amenities: req.body.amenities,
-//       categories: req.body.categories,
-//       foodNightlife: req.body.foodNightlife,
-//       attractions: req.body.attractions,
-//       // Add other fields as needed
-//     });
-
-//     await post.save();
-
-//     res.send(post);
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send({ message: "Internal Server Error" });
-//   }
-// });
-
 // delete a post
 
-imagesRouter.delete("/post/:id", async (req, res) => {
+imagesRouter.delete("/:id", async (req, res) => {
   try {
     const id = req.params.id.toString();
 
